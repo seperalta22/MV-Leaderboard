91d4d5b839483f25a48b59dc9b1a28859c148de7
Sergio Peralta <seperalta88@gmail.com>
2023-05-16 19:24:11 -0300

    Add post function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './styles.css';
+import addRecord from './modules/addRecord.js';
+
+const user = document.getElementById('userName');
+const score = document.getElementById('userScore');
+const submit = document.getElementById('submit');
+
+submit.addEventListener('click', (e) => {
+  e.preventDefault();
+  addRecord(user.value, score.value);
+});
